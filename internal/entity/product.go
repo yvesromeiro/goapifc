62584f9866dc2bc27604284dc52fa1203a00c5cc
Yves <yvesromeiro@icloud.com>
2023-06-05 13:04:24 -0300

    infra

--- internal/entity/product.go
+++ internal/entity/product.go
@@ -3,22 +3,53 @@ package entity
 import (
 	"errors"
 	"github.com/yvesromeiro/goapifc/pkg/entity"
+	"time"
 )
 
 var (
-	errIDIsRequired    = errors.New("id is required")
-	errInvalidID       = errors.New("invalid id")
-	errNameIsRequired  = errors.New("name is required")
-	errPriceIsRequired = errors.New("price is required")
+	ErrIDIsRequired    = errors.New("id is required")
+	ErrInvalidID       = errors.New("invalid id")
+	ErrNameIsRequired  = errors.New("name is required")
+	ErrPriceIsRequired = errors.New("price is required")
+	ErrInvalidPrice    = errors.New("invalid price")
 )
 
 type Product struct {
-	ID       entity.ID `json:"id"`
-	Name     string    `json:"name"`
-	Price    float64   `json:"price"`
-	CreateAt string    `json:"create_at"`
+	ID        entity.ID `json:"id"`
+	Name      string    `json:"name"`
+	Price     float64   `json:"price"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+func NewProduct(name string, price float64) (*Product, error) {
+	product := &Product{
+		ID:        entity.NewID(),
+		Name:      name,
+		Price:     price,
+		CreatedAt: time.Now(),
+	}
+	err := product.Validate()
+	if err != nil {
+		return nil, err
+	}
+	return product, nil
 }
 
 func (p *Product) Validate() error {
-
+	if p.ID.String() == "" {
+		return ErrIDIsRequired
+	}
+	if _, err := entity.ParseID(p.ID.String()); err != nil {
+		return ErrInvalidID
+	}
+	if p.Name == "" {
+		return ErrNameIsRequired
+	}
+	if p.Price == 0 {
+		return ErrPriceIsRequired
+	}
+	if p.Price < 0 {
+		return ErrInvalidPrice
+	}
+	return nil
 }
